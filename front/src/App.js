25c31d63e759fd751b4ec1f6eb537f756eda26a9
kimmoperala <kimmoperala@gmail.com>
2021-10-06 10:56:17 +0300

    Added the right files

--- front/src/App.js
+++ front/src/App.js
@@ -1,29 +1,14 @@
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
-import Home from "./pages/Home";
-import Calendar from "./pages/Calendar";
-import AllMessages from "./pages/AllMessages";
+
 import Layout from "./components/layout/Layout";
-import Admin from "./pages/Admin";
 import Github from "./pages/Github";
 
 function App() {
   return (
     <Layout>
       <Switch>
-        {/*<Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/kalenteri">
-          <Calendar />
-        </Route>
-        <Route path="/viestit">
-          <AllMessages />
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>*/}
         <Route path="/github">
           <Github />
         </Route>
